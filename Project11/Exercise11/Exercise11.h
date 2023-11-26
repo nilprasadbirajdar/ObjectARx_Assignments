a42ac49cf2198e552bc057627b6f3e00149e979f
nilprasadbirajdar <149322004+nilprasadbirajdar@users.noreply.github.com>
2023-11-26 22:18:51 +0530

    Add files via upload

---
+++ Project11/Exercise11/Exercise11.h
@@ -0,0 +1,28 @@
+// Exercise11.h : main header file for the Exercise11 DLL
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+#include "resource.h"		// main symbols
+#include <Windows.h>
+#include<arxHeaders.h>
+#include <dbents.h>
+
+// CExercise11App
+// See Exercise11.cpp for the implementation of this class
+//
+
+class CExercise11App : public CWinApp
+{
+public:
+	CExercise11App();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	static void commandAndSystemVariable();
+	DECLARE_MESSAGE_MAP()
+};
